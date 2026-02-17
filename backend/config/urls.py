98bfe3b7a24820f3831c8133399697efacd10347
zyambo <mightypz2@gmail.com>
2026-02-17 12:07:49 +0200

    test: Add unittests

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api/v1/creators/', include('apps.creators.urls')),
     path('api/v1/wallets/', include('apps.wallets.urls')),
     path('api/v1/payments/', include('apps.payments.urls')),
+    path('payouts/', include('apps.payouts.urls')),
     # OpenAI Schema and Documentation
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
