2c7eaa5bb8139f3dc96d1f491c0a91a14741b7b1
sepiso <mukelabaisepiso@gmail.com>
2026-02-18 21:38:34 +0200

    chore: Move service layer tests to one test module to match project structure

--- backend/tests/test_wallets/test_services/test_wallet_services.py
+++ backend/tests/test_wallets/test_services/test_wallet_services.py
@@ -1,12 +1,15 @@
-from decimal import Decimal
 import pytest
-from apps.wallets.services.transaction_service import\
+from decimal import Decimal
+from apps.wallets.services.wallet_services import WalletService
+from apps.wallets.services.wallet_services import\
     WalletTransactionService as WalletTxnService
 from utils.exceptions import (
-    InsufficientBalance, DuplicateTransaction, InvalidTransaction)
+    InsufficientBalance, DuplicateTransaction,
+    InvalidTransaction,WalletNotFound, WalletError)
+from tests.factories import UserFactory
 
 
-class TestWalletService:
+class TestWalletTransactionService:
     """Test Single source of truth for all wallet money movements."""
 
     def test_wallet_balance_matches_transactions(self, user_factory):
@@ -252,3 +255,65 @@ class TestWalletService:
         with pytest.raises(Exception):
             WalletTxnService.finalize_payout(
                 payout_tx=tx, success=True)
+
+
+@pytest.mark.django_db
+class TestWalletService:
+    def test_get_wallet_for_user(self, user_factory):
+        wallet = WalletService.get_wallet_for_user(user_factory)
+        assert wallet == user_factory.creator_profile.wallet
+
+    def test_get_wallet_for_user_with_no_wallet(self):
+        user = UserFactory(user_type="admin")
+
+        with pytest.raises(WalletNotFound, match="User does not have a wallet"):
+            WalletService.get_wallet_for_user(user)
+        
+    def test_recalculate_wallet_balance_cash_in_out(self, wallet_txn_factory):
+        from decimal import Decimal
+        wallet_txn = wallet_txn_factory(
+            amount=Decimal('10'), status="COMPLETED")
+        WalletService.recalculate_wallet_balance(wallet_txn.wallet)
+        wallet_txn.wallet.refresh_from_db()
+        assert wallet_txn.wallet.balance == Decimal('10')
+
+        # Add fee
+        wallet_txn = wallet_txn_factory(
+            amount=Decimal('-3'), wallet=wallet_txn.wallet, status="COMPLETED",
+            transaction_type="FEE")
+        wallet_txn.save()
+        WalletService.recalculate_wallet_balance(wallet_txn.wallet)
+        wallet_txn.wallet.refresh_from_db()
+        assert wallet_txn.wallet.balance == Decimal('10')
+
+        #cashout
+        wallet_txn = wallet_txn_factory(
+            amount=Decimal('-3'), wallet=wallet_txn.wallet, status="COMPLETED",
+            transaction_type="PAYOUT")
+        wallet_txn.save()
+        WalletService.recalculate_wallet_balance(wallet_txn.wallet)
+        wallet_txn.wallet.refresh_from_db()
+        assert wallet_txn.wallet.balance == Decimal('7')
+
+    def test_dont_add_pending_txn_to_balance(self, wallet_txn_factory):
+        from decimal import Decimal
+        wallet_txn = wallet_txn_factory(
+            amount=Decimal('10'), status="COMPLETED")
+        WalletService.recalculate_wallet_balance(wallet_txn.wallet)
+        wallet_txn.wallet.refresh_from_db()
+        assert wallet_txn.wallet.balance == Decimal('10')
+
+        wallet_txn = wallet_txn_factory(
+            amount=Decimal('-3'), wallet=wallet_txn.wallet)
+        wallet_txn.save()
+        WalletService.recalculate_wallet_balance(wallet_txn.wallet)
+        wallet_txn.wallet.refresh_from_db()
+        assert wallet_txn.wallet.balance == Decimal('10')
+
+    def test_calculate_balance_bad_arguments(self, wallet_txn_factory):
+        from decimal import Decimal
+        wallet_txn_factory(
+            amount=Decimal('10'), status="COMPLETED")
+        with pytest.raises(WalletError):
+            WalletService.recalculate_wallet_balance('not-wallet')
+        
