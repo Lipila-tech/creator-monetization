5bd6051859431fd65d7ac27aec92e94d452d235e
zyambo <mightypz2@gmail.com>
2026-04-03 12:51:44 +0200

    fix: Catch unraised exceptions and remove redundant fields from serializer

--- backend/apps/customauth/firebase.py
+++ backend/apps/customauth/firebase.py
@@ -6,6 +6,6 @@ from firebase_admin import credentials
 def initialize_firebase():
     if not firebase_admin._apps:
         cred_path = os.getenv("FIREBASE_CREDENTIALS")
-        cred_dict = json.loads(cred_path)
+        cred_dict = json.loads(cred_path, strict=False)
         cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
